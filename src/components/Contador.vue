ab8e4deb24d1a5ee4473cfbdc1f8138810928213
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-22 20:54:57 -0400

    modifico componente
    edito contador

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,38 +1,36 @@
 <template>
- <div class="container" id="contador">
-<h6>Propiedad Computada</h6>
- <h1>Contador: {{cantidad}}</h1>
- <v-btn rounded color="blue" @click="aumentar">Aumentar</v-btn> |
-  <v-btn rounded color="error" @click="reducir">Reducir</v-btn>
- </div>
+  <div class="container" id="contador">
+    <h6>Propiedad Computada</h6>
+    <h3>Contador:</h3>
+    <h1>{{cantidad}}</h1>
+    <v-btn x-small rounded color="blue" @click="aumentar">Aumentar</v-btn>|
+    <v-btn x-small rounded color="error" @click="reducir">Reducir</v-btn>
+  </div>
 </template>
 
 <script>
 export default {
-
-//props: ['cantidad'],
-methods: {
-    aumentar(){
-        //this.$emit('aumentar');
-        this.$store.state.cantidad++;
+  methods: {
+    aumentar() {
+      this.$store.state.cantidad++;
     },
-    reducir(){
-        //this.$emit('reducir');
-        this.$store.state.cantidad--;
-    }
-},
+    reducir() {
+      this.$store.state.cantidad--;
+    },
+  },
 
-computed: {
-    cantidad(){
-        return this.$store.state.cantidad;
-    }
-},
-
-}
+  computed: {
+    cantidad() {
+      return this.$store.state.cantidad;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-#contador{
-    font-family: 'Courier New', Courier, monospace;
+#contador {
+  font-family: "Courier New", Courier, monospace;
+  border: 2px red solid;
+  text-align: center;
 }
 </style>
